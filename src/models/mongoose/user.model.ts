b142694947e16b3eb10752b7041f3351d8716b00
Amir Allahdadian <amir.allahdadian@gmail.com>
2024-06-06 06:16:41 +0330

    created user model

---
+++ src/models/mongoose/user.model.ts
@@ -0,0 +1,40 @@
+import { Schema, model } from "mongoose";
+import MongooseDelete from "mongoose-delete";
+
+const userSchema = new Schema(
+	{
+		firstName: { type: Schema.Types.String, required: true },
+		lastName: { type: Schema.Types.String, required: true },
+		email: { type: Schema.Types.String, required: true, unique: true },
+		phoneNumber: { type: Schema.Types.String, required: false, unique: true },
+		password: { type: Schema.Types.String, required: true },
+		picture: { type: Schema.Types.String, required: false },
+		// parentalAdvisory: { type: Schema.Types.Boolean, default: false },
+		// favorite: { type: Schema.Types.Boolean, default: false },
+		// mostPlayed: { type: Schema.Types.Boolean, default: false },
+		// new: { type: Schema.Types.Boolean, default: true },
+		// title: { type: Schema.Types.String, required: true },
+		// artist: { type: Schema.Types.String, required: true },
+		// coArtists: { type: [Schema.Types.String] },
+		// album: { type: Schema.Types.String },
+		// image: { type: Schema.Types.String },
+		// file: { type: Schema.Types.String, required: true, unique: true },
+	},
+	{
+		timestamps: true,
+		virtuals: {
+			fullName: {
+				get() {
+					return this.firstName + " " + this.lastName;
+				},
+			},
+		},
+		toJSON: { virtuals: true },
+	}
+);
+
+userSchema.plugin(MongooseDelete, { deletedAt: true, deletedBy: true });
+
+const User = model("User", userSchema);
+
+export default User;
